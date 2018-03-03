4f0f8b462f95b55b81717f8e29e70e192ddee82f
kubotak-is <kubotak@istyle.co.jp>
2018-03-03 20:01:34 +0900

    modify: edotor.js

--- kk-blog-card-shortcode.php
+++ kk-blog-card-shortcode.php
@@ -3,7 +3,7 @@
 add_shortcode('blog-card', function($atts) {
   extract(shortcode_atts([
     'href' => '/',
-    'type' => 'default',
+    'type' => 'fb',
   ], $atts));
   return '<blog-card href="'.$href.'" data-type="'.$type.'"></blog-card>';
 });
@@ -12,7 +12,7 @@ add_action('admin_print_footer_scripts', function() {
   if (wp_script_is('quicktags')) {
   ?>
   <script type="text/javascript">
-  QTags.addButton('blog-card', 'blog-card', '[blog-card href=""]', '');
+  QTags.addButton('blog-card', 'blog-card', '[blog-card href="" type=""]', '');
   </script>
   <?php
   }
@@ -28,6 +28,6 @@ function blogcard_plugin_register_buttons($buttons) {
 add_filter('mce_external_plugins', 'blogcard_plugin_register_tinymce_javascript');
 
 function blogcard_plugin_register_tinymce_javascript($plugin_array) {
-   $plugin_array['blogcard_plugin_script'] = plugins_url('/editor-btn.js', __FILE__);
+   $plugin_array['blogcard_plugin_script'] = plugins_url('/editor.js', __FILE__);
    return $plugin_array;
 }
